18920f07877dc706c6663e491777fd9d38a7b08f
kashiwara <tanakatarou0205@gmail.com>
2021-09-18 16:31:55 +0900

    Add welcome component

--- react-tutorial/app/src/App.js
+++ react-tutorial/app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Welcome from './component/welcome'
 
 function App() {
   const text = "hello world"
   return (
     <div className="App">
       <p> { text } </p>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
     </div>
   );
 }
